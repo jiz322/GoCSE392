f209fed5659b4288290deacd0505528315d8d0dc
jiz322 <jiz322@lehigh.edu>
2021-06-20 16:39:54 -0400

    update pit

--- Go Implementation/pit.py
+++ Go Implementation/pit.py
@@ -19,7 +19,7 @@ human_vs_cpu = True
 if mini_othello:
     g = OthelloGame(6)
 else:
-    g = OthelloGame(8)
+    g = OthelloGame(5)
 
 # all players
 rp = RandomPlayer(g).play
@@ -31,9 +31,9 @@ hp = HumanOthelloPlayer(g).play
 # nnet players
 n1 = NNet(g)
 if mini_othello:
-    n1.load_checkpoint('./pretrained_models/othello/pytorch/','6x100x25_best.pth.tar')
+    n1.load_checkpoint('./temp/','_best.pth.tar')
 else:
-    n1.load_checkpoint('./pretrained_models/othello/pytorch/','8x8_100checkpoints_best.pth.tar')
+    n1.load_checkpoint('./temp/','best.pth.tar')
 args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
 mcts1 = MCTS(g, n1, args1)
 n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
@@ -42,7 +42,7 @@ if human_vs_cpu:
     player2 = hp
 else:
     n2 = NNet(g)
-    n2.load_checkpoint('./pretrained_models/othello/pytorch/', '8x8_100checkpoints_best.pth.tar')
+    n2.load_checkpoint('./temp/', 'best.pth.tar')
     args2 = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
     mcts2 = MCTS(g, n2, args2)
     n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
