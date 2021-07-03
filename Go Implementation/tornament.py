1164f80697a32101e1928a6f92dc6f0ce8f072aa
jiz322 <jiz322@lehigh.edu>
2021-07-03 17:20:04 -0400

    add tornament

---
+++ Go Implementation/tornament.py
@@ -0,0 +1,94 @@
+import Arena
+from MCTS import MCTS
+from othello.OthelloGame import OthelloGame
+from othello.OthelloPlayers import *
+from othello.pytorch.NNet import NNetWrapper as NNet
+
+from varname import nameof
+import numpy as np
+import random
+import operator
+from utils import *
+
+"""
+use thisss script to play any two agents against each other, or play manually with
+any agent.
+"""
+
+human_vs_cpu = True
+
+
+g = OthelloGame(5)
+
+# all players
+rp = RandomPlayer(g).play
+#gp = GreedyOthelloPlayer(g).play
+hp = HumanOthelloPlayer(g).play
+
+
+def createNetPlayer(tarName, sim=2, cpuct=1.1):
+    n = NNet(g)
+    n.load_checkpoint('./temp/', tarName)
+    args = dotdict({'numMCTSSims': sim, 'cpuct':cpuct})
+    mcts = MCTS(g, n, args)
+    player = lambda x: np.argmax(mcts.getActionProb(x, temp=0)[0])
+    return player
+
+def playGame(player1, str1, player2, str2):
+    arena = Arena.Arena(player1, player2, g, display=OthelloGame.display)
+    x, y, z, xb = arena.playGames(100, verbose=True)
+    print(str1, " win: ", x)
+    print(str2, " win: ", y)
+    return x, y
+#playGame(best1, 'best', best2, 'best2')
+
+def tournament(playList):
+    tournamentResult = dict.fromkeys(playList, 0)
+    for a in playList:
+        for b in playList:
+            if a is not b:
+                aWin, bWin = playGame(a, 'p1', b, 'p2')           
+                tournamentResult[a] += (aWin - bWin + 100)
+                tournamentResult[b] += (bWin - aWin + 100)
+    print (
+    '''
+    く__,.ヘヽ.　　　　/　,ー､ 〉
+    　　　＼ , !-─‐-i　/　/´
+    　　　 　 ／｀ｰ　　　 L/／｀ヽ､
+    　　 　 /　 ／,　 /|　 ,　 ,　　    ,
+    　　　ｲ 　/ /-‐/　ｉ　L_ ﾊ ヽ!　 i
+    　　　 ﾚ ﾍ 7ｲ｀ﾄ　 ﾚ-ﾄ､!ハ|　 |
+    　　　　 !,/7 ✪　　 ´i✪ﾊiソ| 　 |　　　
+    　　　　 |.从　　_　　 ,,,, / |./ 　 |
+    　　　　 ﾚ| i＞.､,,__　_,.イ / 　.i 　|
+    　　　 ﾚ| | / k_７_/ﾚヽ,　ﾊ.　|
+    　　　 | |/i 〈|/　 i　,.ﾍ |　i　|
+    　　　.|/ /　ｉ： 　 ﾍ!　　＼　|
+    　　　 　 　 kヽ､ﾊ 　 _,.ﾍ､ 　 /､!
+    　　　 !〈//｀Ｔ´, ＼ ｀7ｰr
+    　　　 ﾚヽL__|___i,___,ンﾚ|ノ
+    　　　 　　　ﾄ-,/　|___./
+    　　　 　　　ｰ　　!_,.:
+    '''
+    )
+    
+
+    return list(tournamentResult.values())
+best = createNetPlayer("best.pth.tar")
+def_27 = createNetPlayer("def_27.pth.tar")
+def_52 = createNetPlayer("def_52.pth.tar")
+def_77 = createNetPlayer("def_77.pth.tar")
+def_102 = createNetPlayer("def_102.pth.tar")
+cha_27 = createNetPlayer("cha_27.pth.tar")
+cha_52 = createNetPlayer("cha_52.pth.tar")
+cha_77 = createNetPlayer("cha_77.pth.tar")
+cha_102 = createNetPlayer("cha_102.pth.tar")
+playerList = [cha_102, def_102]
+result = tournament(playerList)
+print(result)
+
+
+            
+               
+                
+
