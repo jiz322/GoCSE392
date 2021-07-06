e2e9dcf22a507a4296e08c1ca78096c9619038eb
jiz322 <jiz322@lehigh.edu>
2021-07-05 23:01:51 -0400

    for 9*9

--- Go Implementation/main.py
+++ Go Implementation/main.py
@@ -12,28 +12,28 @@ log = logging.getLogger(__name__)
 coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
 
 args = dotdict({
-    'numIters': 10,
-    'numEps': 100,              # Number of complete self-play games to simulate during a new iteration.
-    'tempThreshold': 1500,        # infite
-    'updateThreshold': 0.60,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
+    'numIters': 200,
+    'numEps': 50,              # Number of complete self-play games to simulate during a new iteration.
+    'tempThreshold': 25,        # infite
+    'updateThreshold': 0.55,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
     'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
-    'numMCTSSims': 150,          # Number of games moves for MCTS to simulate.
-    'arenaCompare': 8,         # Number of games to play during arena play to determine if new net will be accepted.
+    'numMCTSSims': 2,          # Number of games moves for MCTS to simulate.
+    'arenaCompare': 100,         # Number of games to play during arena play to determine if new net will be accepted.
     'cpuct': 1.1,
-    'arenaNumMCTSSims': 52,
-    'firstIter': False,        #set true if it produce first chechpoint to save, for multuprocess, the following has to be FALSE
+    'arenaNumMCTSSims': 2,
+    'firstIter': True,        #set true if it produce first chechpoint to save, for multuprocess, the following has to be FALSE
 
     'checkpoint': './temp/',
-    'load_model': True,
+    'load_model': False,
     'load_folder_file': ('./temp','77.pth.tar'),
-    'numItersForTrainExamplesHistory': 50,
+    'numItersForTrainExamplesHistory': 20,
 
 })
 
 
 def main():
     log.info('Loading %s...', Game.__name__)
-    g = Game(5)
+    g = Game(9)
 
     log.info('Loading %s...', nn.__name__)
     nnet = nn(g)
