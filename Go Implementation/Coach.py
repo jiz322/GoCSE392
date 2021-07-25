37d1756c9fd766810f97f7b29d2060088e7dd5d3
jiz322 <jiz322@lehigh.edu>
2021-07-25 12:46:35 -0400

    public branch

--- Go Implementation/Coach.py
+++ Go Implementation/Coach.py
@@ -25,50 +25,12 @@ class Coach():
         self.game = game
         self.nnet = nnet
         self.pnet = self.nnet.__class__(self.game)  # the competitor network
+        self.n2net = self.nnet.__class__(self.game)  # the competitor network
+        self.n3net = self.nnet.__class__(self.game)  # the competitor network
+        self.n4net = self.nnet.__class__(self.game)  # the competitor network
         self.args = args
         self.mcts = MCTS(self.game, self.nnet, self.args)
         self.trainExamplesHistory = []  # history of examples from args.numItersForTrainExamplesHistory latest iterations
-        self.skipFirstSelfPlay = False  # can be overriden in loadTrainExamples()
-
-    def executeEpisode(self):
-        """
-        This function executes one episode of self-play, starting with player 1.
-        As the game is played, each turn is added as a training example to
-        trainExamples. The game is played till the game ends. After the game
-        ends, the outcome of the game is used to assign values to each example
-        in trainExamples.
-
-        It uses a temp=1 if episodeStep < tempThreshold, and thereafter
-        uses temp=0.
-
-        Returns:
-            trainExamples: a list of examples of the form (canonicalBoard, currPlayer, pi,v)
-                           pi is the MCTS informed policy vector, v is +1 if
-                           the player eventually won the game, else -1.
-        """
-        trainExamples = []
-        board = self.game.getInitBoard()
-        self.curPlayer = 1
-        episodeStep = 0
-
-        while True:
-            episodeStep += 1
-            canonicalBoard = self.game.getCanonicalForm(board, self.curPlayer)
-            temp = int(episodeStep < self.args.tempThreshold)
-            #fastDecision data should not be collected (ketaGo Paper)
-            pi, fastDecision, resign = self.mcts.getActionProb(canonicalBoard, temp=temp, training=1)
-            sym = self.game.getSymmetries(canonicalBoard, pi)
-            for b, p in sym:
-                if not fastDecision:  #only add example of slow decisions
-                    trainExamples.append([b, self.curPlayer, p, None])
-
-            action = np.random.choice(len(pi), p=pi)
-            board, self.curPlayer = self.game.getNextState(board, self.curPlayer, action)
-            r = self.game.getGameEnded(board, self.curPlayer)
-            if resign:
-                r = 1 # previous player resigned          
-            if r != 0:
-                return [(x[0], x[2], r * ((-1) ** (x[1] != self.curPlayer))) for x in trainExamples]
 
     def learn(self):
         """
@@ -88,85 +50,55 @@ class Coach():
             log.info(f'Starting Iter #{i} ...')
             self.trainExamplesHistory = [] # empty the history 
             # examples of the iteration
-            if not self.skipFirstSelfPlay or i > 1:
-                iterationTrainExamples = deque([], maxlen=self.args.maxlenOfQueue)
-                for _ in tqdm(range(self.args.numEps), desc="Self Play"):
-                    self.mcts = MCTS(self.game, self.nnet, self.args)  # reset search tree
-                    iterationTrainExamples += self.executeEpisode()
-                # save the iteration examples to the history 
-                self.trainExamplesHistory.append(iterationTrainExamples)
-            if len(self.trainExamplesHistory) > self.args.numItersForTrainExamplesHistory:
-                log.warning(
-                    f"Removing the oldest entry in trainExamples. len(trainExamplesHistory) = {len(self.trainExamplesHistory)}")
-                self.trainExamplesHistory.pop(0)
-            # backup history to a file
-            # NB! the examples were collected using the model from the previous iteration, so (i-1)  
-            self.saveTrainExamples()
-            self.loadExamples() #see if this get larger
+            self.loadExamples() #Load the best.pth.tar.examples
             # shuffle examples before training
             trainExamples = []
             for e in self.trainExamplesHistory:
                 trainExamples.extend(e)
-            shuffle(trainExamples)
-            self.nnet.save_checkpoint(folder=self.args.checkpoint, filename='temp.pth.tar')# training new network, keeping a copy of the old one              
+            self.nnet.load_checkpoint(folder=self.args.checkpoint, filename='best.pth.tar') 
+            self.n2net.load_checkpoint(folder=self.args.checkpoint, filename='best.pth.tar') 
+            self.n3net.load_checkpoint(folder=self.args.checkpoint, filename='best.pth.tar') 
+
+            shuffle(trainExamples)              
             self.nnet.train(trainExamples)
+            shuffle(trainExamples)              
+            self.n2net.train(trainExamples)
+            shuffle(trainExamples)              
+            self.n3net.train(trainExamples)
+            self.n3net.save_checkpoint(folder=self.args.checkpoint, filename='temp.pth.tar')
+            self.n4net.load_checkpoint(folder=self.args.checkpoint, filename='best.pth.tar') #Load n3 result, see if it perform better
+            shuffle(trainExamples)              
+            self.n4net.train(trainExamples)
+
             nmcts = MCTS(self.game, self.nnet, self.args)
-            if not self.firstIter:
-                self.pnet.load_checkpoint(folder=self.args.checkpoint, filename='best.pth.tar') #Load the best after training to maximize efficenty
-                pmcts = MCTS(self.game, self.pnet, self.args)
-                log.info('PITTING AGAINST PREVIOUS VERSION')
-                arena = Arena(lambda x: np.argmax(pmcts.getActionProb(x, temp=1, arena=1, instinctPlay=self.args.instinctArena)[0]),
-                            lambda x: np.argmax(nmcts.getActionProb(x, temp=1, arena=1, instinctPlay=self.args.instinctArena)[0]), self.game)
-                pwins, nwins, draws, pwins_black = arena.playGames(self.args.arenaCompare)
-                log.info('NEW/PREV WINS : %d / %d ; DRAWS : %d ; PREV_WinOnBlack : %d' % (nwins, pwins, draws, pwins_black))
-                if pwins + nwins == 0 or float(nwins) / (pwins + nwins) < self.args.updateThreshold:
-                    log.info('REJECTING NEW MODEL')
-                    #load the current best after reject
-                    self.nnet.load_checkpoint(folder=self.args.checkpoint, filename='best.pth.tar')
-                    #self.loadTrainExamples(loadBest=True) 
-                    # Keep the example the same!!!!!!!
-                else:
-                    log.info('ACCEPTING NEW MODEL')
-                    self.nnet.save_checkpoint(folder=self.args.checkpoint, filename=self.getCheckpointFile(i))
-                    self.nnet.save_checkpoint(folder=self.args.checkpoint, filename='best.pth.tar')
+            n2mcts = MCTS(self.game, self.n2net, self.args)
+            n3mcts = MCTS(self.game, self.n3net, self.args)
+            n4mcts = MCTS(self.game, self.n4net, self.args)
 
-            else: #first iteratioin, just accept it so that we have best.pth.tar
-                log.info('ACCEPTING NEW MODEL')
-                self.nnet.save_checkpoint(folder=self.args.checkpoint, filename=self.getCheckpointFile(i))
-                self.nnet.save_checkpoint(folder=self.args.checkpoint, filename='best.pth.tar')
-                self.firstIter = False
+            self.pnet.load_checkpoint(folder=self.args.checkpoint, filename='best.pth.tar') #Load the best after training to maximize efficenty
+            pmcts = MCTS(self.game, self.pnet, self.args)
+            log.info('Arena Start! A tournament between PREVIOUS VERSION and 4 new ones!')
 
+            player_prev = lambda x: np.argmax(pmcts.getActionProb(x, temp=0)[0])
+            player_n = lambda x: np.argmax(nmcts.getActionProb(x, temp=0)[0])
+            player_n2 = lambda x: np.argmax(n2mcts.getActionProb(x, temp=0)[0])
+            player_n3 = lambda x: np.argmax(n3mcts.getActionProb(x, temp=0)[0])
+            player_n4 = lambda x: np.argmax(n4mcts.getActionProb(x, temp=0)[0])
+
+            playerList = [player_prev, player_n, player_n2, player_n3, player_n4]
+            mctsList = [pmcts, nmcts, n2mcts, n3mcts, n4mcts]
+            nnList = [self.pnet, self.nnet, self.n2net, self.n3net, self.n4net]
+            result = self.tournament(playerList)
+            winner = nnList[result.index(max(result))]
+            winner.save_checkpoint(folder=self.args.checkpoint, filename='best.pth.tar')
+            log.info('UPDATING BEST')
+            log.info('ARENA RESULT: ', result)
+            winner.save_checkpoint(folder=self.args.checkpoint, filename=self.getCheckpointFile(i))
+
+                
     def getCheckpointFile(self, iteration):
         return 'checkpoint_' + str(iteration) + '.pth.tar'
 
-    #add the way to save best example
-    def saveTrainExamples(self):
-        folder = self.args.checkpoint
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-        filename = os.path.join(folder, "best.pth.tar.examples")
-        with open(filename, "ab+") as f:
-            Pickler(f).dump(self.trainExamplesHistory)
-        f.closed
-
-    #add the way to load best example
-    def loadTrainExamples(self, loadBest=False):
-        modelFile = os.path.join(self.args.load_folder_file[0], self.args.load_folder_file[1])
-        examplesFile = modelFile + ".examples"
-        if loadBest:
-            examplesFile = "best.pth.tar.examples"
-        if not os.path.isfile(examplesFile):
-            log.warning(f'File "{examplesFile}" with trainExamples not found!')
-        else:
-            log.info("File with trainExamples found. Loading it...")
-            with open(examplesFile, "rb") as f:
-                self.trainExamplesHistory = Unpickler(f).load()
-            log.info('Loading done!')
-
-            # examples based on the model were already collected (loaded)
-            # do not skip if it loads the best
-            if not loadBest:
-                self.skipFirstSelfPlay = True
     #load examples only
     def loadExamples(self):
         folder = self.args.checkpoint
@@ -187,5 +119,24 @@ class Coach():
             self.trainExamplesHistory.pop(0)
         log.info('Loading done!')
 
+    def playGame(player1, str1, player2, str2):
+        arena = Arena.Arena(player1, player2, g)
+        x, y, z, xb = arena.playGames(args.arenaCompare, verbose=False)
+        print(str1, " win: ", x)
+        print(str2, " win: ", y)
+        print(str1, " win black: ", xb)
+        return x, y
 
-
+    def tournament(playList):
+        tournamentResult = dict.fromkeys(playList, 0)
+        repeat = [] #each player only play 2 games with each other
+        for a in playList:
+            for b in playList:
+                #this 
+                if playList.index(a)+1)*(playList.index(b)+1) not in repeat:
+                    if a is not b:
+                        aWin, bWin = self.playGame(a, 'p1', b, 'p2')           
+                        tournamentResult[a] += (aWin - bWin + args.arenaCompare)/2
+                        tournamentResult[b] += (bWin - aWin + args.arenaCompare)/2
+                        repeat.append((playList.index(a)+1)*(playList.index(b)+1))
+        return list(tournamentResult.values())
--- Go Implementation/main.py
+++ Go Implementation/main.py
@@ -13,23 +13,18 @@ coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
 
 args = dotdict({
     'size': 9,                  #board size
-    'numIters': 100,
-    'numEps': 10,              # Number of complete self-play games to simulate during a new iteration.
-    'tempThreshold': 30,        # infite
-    'updateThreshold': 0.55,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
-    'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
-    'numMCTSSims': 800,          # Number of games moves for MCTS to simulate.
-    'arenaCompare': 10,         # Number of games to play during arena play to determine if new net will be accepted.
+    'tempThreshold': 0,        # zero
+    'numMCTSSims': 200,          # Number of games moves for MCTS to simulate.
+    'arenaNumMCTSSims': 200,      #simulation for arena
+    'arenaCompare': 2,         # Tornament version alsways 2
     'cpuct': 1.1,
-    'arenaNumMCTSSims': 52,      #simulation for arena
     'instinctArena': False,     #if set true reset Arena's MTCL tree each time
     'balancedGame': True,      # if balanced, black should win over 6 scores
     'firstIter': False,        #set true if it produce first chechpoint to save, for multuprocess, the followings has to be FALSE
     'checkpoint': './temp/',
-    'load_model': True,
     'load_folder_file': ('./temp','best.pth.tar'),
-    'numItersForTrainExamplesHistory': 100,
-    'resignThreshold': -2   #resign when best Q value less than threshold Q[-1, 1]
+    'numItersForTrainExamplesHistory': 1000,
+    'resignThreshold': -2   #resign does not work for Arena
 
 })
 
@@ -41,18 +36,8 @@ def main():
     log.info('Loading %s...', nn.__name__)
     nnet = nn(g)
 
-    if args.load_model:
-        log.info('Loading checkpoint "%s/%s"...', args.load_folder_file)
-        nnet.load_checkpoint(args.load_folder_file[0], args.load_folder_file[1])
-    else:
-        log.warning('Not loading a checkpoint!')
-
     log.info('Loading the Coach...')
     c = Coach(g, nnet, args)
-
-    if args.load_model:
-        log.info("Loading 'trainExamples' from file...")
-        c.loadTrainExamples()
 
     log.info('Starting the learning process 🎉')
     c.learn()
